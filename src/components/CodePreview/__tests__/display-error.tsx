dbe977ea76e6593b99a6e7b43eec80f4a0493a58
Dustin Schau <dustinschau@gmail.com>
2017-09-10 00:15:50 -0500

    test: add a bunch more unit tests
    Fixes #15

--- src/components/CodePreview/__tests__/display-error.tsx
+++ src/components/CodePreview/__tests__/display-error.tsx
@@ -3,12 +3,10 @@ import { shallow } from 'enzyme';
 
 import DisplayError from '../display-error';
 
-test('it renders an error', () => {
-  const wrapper = shallow(<DisplayError error={{
+test('it renders', () => {
+  expect(() => shallow(<DisplayError error={{
     name: 'hello-world'
   }} errorInfo={{
     componentStack: 'asdf'
-  }}/>);
-
-  console.log(wrapper.html());
+  }} />)).not.toThrow();
 });
